9463052360bf1efd301a25def2dc64c60bcc4c66
chad skeeters <chad.skeeters@gmail.com>
2016-12-16 14:14:10 -0600

    Added comment on modifying base16 theme in rouge

--- public/rouge-css.rb
+++ public/rouge-css.rb
@@ -1,5 +1,28 @@
 #!/usr/local/bin/ruby
 
+# Some pygment names might be missing from colorization.  For example 'cd' in
+# bash was being span'ed with 'nb', but the Base16 render method was not
+# outputing anything for nb.
+#
+# To resolve this, you can edit the installed gem
+# GEM_PATH/gems/rouge-2.X.X/lib/rouge/themes/base16.rb
+#
+# Your'll notice there is no `style Name` or `style Name::Builtin` to match the
+# Name.Builtin from pygments.  To correct for this you can modify the Name
+# blocks with this:
+#
+#      style Name::Builtin, :fg => :base0D
+#      style Name::Variable, :fg => :base08
+#      style Name::Namespace,
+#            Name::Class,
+#            Name::Constant, :fg => :base0A
+#
+#      style Name::Attribute, :fg => :base0D
+#
+# Color Reference to tokens: http://chriskempson.com/projects/base16/
+# Pygment tokens: http://pygments.org/docs/tokens/
+
+
 require_relative 'md_common'
 
 begin
